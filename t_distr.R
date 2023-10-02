531424614bf737247109472ad7f1d431566db939
David Chen <nocktavius@gmail.com>
2023-10-02 14:29:57 +0200

    added r code and plot of normal distr with t distr with different degrees of freedom

---
+++ t_distr.R
@@ -0,0 +1,26 @@
+library("ggplot2")
+library(hrbrthemes)
+
+pdf(file = "C:/Users/Admin/Desktop/math-stat/tdistr.pdf",   # The directory you want to save the file in
+    width = 6, # The width of the plot in inches
+    height = 5) # The height of the plot in inches
+
+curve(dnorm(x), 
+      from=-3, to=3, col=1, lwd=3, lty=6, 
+      ylab= substitute(paste(bold('f(x)'))),
+      xlab= substitute(paste(bold('x')))
+)
+curve(dt(x, 8), from=-3, to=3, add=TRUE, col=5, lwd=3, lty=1)
+curve(dt(x, 3), from=-3, to=3, add=TRUE, col=6, lwd=3, lty=2)
+curve(dt(x, 1), from=-3, to=3, add=TRUE, col=7, lwd=3, lty=4)
+
+legend(x = "topright",         
+       legend = c(substitute(paste(bold("normal"))),
+                  substitute(paste(bold("r=8"))),
+                  substitute(paste(bold("r=3"))),
+                  substitute(paste(bold("r=1")))),  
+       lty = c(6,1,2,4),           
+       col = c(1,5,6,7),      
+       lwd = 3)            
+
+dev.off()
