1ac5697c2f756d73af63f3897b84ad01345ac132
David Chen <nocktavius@gmail.com>
2023-10-27 18:11:19 +0200

    formatting changes

--- get_BBBYQ_data.py
+++ get_BBBYQ_data.py
@@ -2,19 +2,21 @@ from datetime import date
 import yfinance as yf
 
 import pandas as pd
+
 # Tickers list
 # We can add and delete any ticker from the list to get desired ticker live data
-ticker=yf.Ticker('BBBYQ')
+ticker = yf.Ticker("BBBYQ")
 
 # We can get data by our choice by giving days bracket
-start_date= "2022-01-01"
-end_date="2023-01-01"
+start_date = "2022-01-01"
+end_date = "2023-01-01"
+
 
 # Create a data folder in your current dir.
 def SaveData(df, filename):
- df.to_csv('./data/'+filename+'.csv')
+    df.to_csv("./data/" + filename + ".csv")
+
 
 data = ticker.history(start=start_date, end=end_date)
-dataname= "BBBYQ"
+dataname = "BBBYQ"
 SaveData(data, dataname)
-
